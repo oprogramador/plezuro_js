cf045ca039252a975a247f78e06e8d5c8c5efef7
oprogramador <uomodislesia@gmail.com>
2015-07-27 03:55:16 +0200

    string token .

--- plezuro_scripts/1.ml
+++ plezuro_scripts/1.ml
@@ -1,5 +1,5 @@
 $x = 21212 + 0.12 + 982 / 12 + 1111.222;
 x++;
 $y = 'abc' + 'abc\nxyz' + 'uu\'bb' + 'ooo';
-y += "mmmmm" + 'cv' + "vfg'ee'ww" + "www\"www\nvvv" + 'ww"ddd"' + "\n";
+y += '' + "mmmmm" + 'cv' + "vfg'ee'ww" + "www\"www\nvvv" + 'ww"ddd"' + "\n";
 ''+x+y
