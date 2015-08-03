1ed6698623a9e2f116efa1abfaf26ce9a65f2f86
oprogramador <uomodislesia@gmail.com>
2015-08-03 23:33:09 +0200

    import

--- js_lib/Import.js
+++ js_lib/Import.js
@@ -1,18 +1,7 @@
-function Import() {
-    var res;
-    var shouldLoad = true;
-
-    function importModule(name) {
-        if(shouldLoad) $.get(name+'.js', function(data) {
-            res = eval(data);
-        }).fail(function(data) {
-            res = new Error('failed to load '+name);  
-        });
-        shouldLoad = false;
-
-        if(res === undefined) setTimeout(importModule, 50);
-        return res;
-    }
-
-    this.importModule = importModule;
+function importModule(name, callback) {
+    $.getScript(name+'.js', function(data, textStatus, jqxhr) {
+        callback(eval(data));
+    }).fail(function(jqxhr, settings, exception) {
+        callback(new Error('failed to load '+name));
+    });
 }
