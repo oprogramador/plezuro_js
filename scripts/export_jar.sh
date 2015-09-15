8b88f8f8523608870f4449861202f47c2fa18f62
oprogramador <uomodislesia@gmail.com>
2015-09-16 00:25:28 +0200

    jar

---
+++ scripts/export_jar.sh
@@ -0,0 +1,5 @@
+cd $(dirname $(realpath $0))
+cd ..
+cd bin
+echo 'Main-Class: mondo.main.Main' > Manifest.txt
+jar cvfm plezuro.jar Manifest.txt -C java/ .
--- scripts/make.sh
+++ scripts/make.sh
@@ -1,4 +1,5 @@
 cd $(dirname $(realpath $0))
 cd ..
 scripts/build.sh &&
+scripts/export_jar.sh &&
 scripts/make_js.sh
