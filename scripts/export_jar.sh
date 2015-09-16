a991b781781a249439e9d4b1139be339777c5a2f
oprogramador <uomodislesia@gmail.com>
2015-09-16 04:45:14 +0200

    auto export

--- scripts/export_jar.sh
+++ scripts/export_jar.sh
@@ -3,3 +3,4 @@ cd ..
 cd bin
 echo 'Main-Class: mondo.main.Main' > Manifest.txt
 jar cvfm plezuro.jar Manifest.txt -C java/ .
+chmod 775 plezuro.jar
--- scripts/make.sh
+++ scripts/make.sh
@@ -2,4 +2,5 @@ cd $(dirname $(realpath $0))
 cd ..
 scripts/build.sh &&
 scripts/export_jar.sh &&
+sudo cp bin/plezuro.jar /usr/bin/
 scripts/make_js.sh
